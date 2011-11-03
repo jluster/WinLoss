86a7941b3ad01ed10827676f460bbc3b68a02e90
Jonas M Luster <j@d8c.us>
2011-11-03 17:08:55 -0500

    Debug code

--- WinLoss.lua
+++ WinLoss.lua
@@ -12,11 +12,13 @@ function wl:WORLD_MAP_UPDATE()
   -- we can reasonably assume that the player has left the game or entered and new one and left
   -- the zone-in area (doesn't fire until you leave WSG hold, for example).
   self.currentBGsaved = 0
+  wl:Debug("Now BGSaved is Zero");
 end
 
 function wl:ADDON_LOADED()
   -- in case someone deletes (as they should) the database
   if WinLossDB == nil then WinLossDB = {} end
+  if WinLossDB["debug"] == nil then WinLossDB["debug"] = false end
 end
 
 function wl:UPDATE_BATTLEFIELD_STATUS()
@@ -27,19 +29,26 @@ function wl:UPDATE_BATTLEFIELD_STATUS()
     local u = UnitLevel("player");
     local s = GetNumBattlefieldScores();
     local zoneName = GetZoneText();
+    wl:Debug("BG Saved is now: "..self.currentBGsaved);
     if self.currentBGsaved == 0 then
       -- Apparently sometimes zone changes aren't triggered so we'll also discount 
       -- any new win within 5 minutes after the last *sigh*
       WinLossDB[d..":"..uname] = winner..":"..u..":"..s..":"..zoneName;
       self.currentBGsaved = 1
+      wl:Debug("Now BGSaved is One");
     end
-    DEFAULT_CHAT_FRAME:AddMessage(winner..":"..u..":"..s..":"..zoneName);
+    wl:Debug(winner..":"..u..":"..s..":"..zoneName);
     if winner == 1 then 
       DEFAULT_CHAT_FRAME:AddMessage("Alliance has won")
     else
       DEFAULT_CHAT_FRAME:AddMessage("Horde has won")
     end
   end
+end
+
+function wl:Debug(message)
+  if not WinLossDB["debug"] then return end
+  DEFAULT_CHAT_FRAME:AddMessage("*** WLD: "..message);
 end
 
 SLASH_WINLOSS1 = "/winloss";
@@ -52,5 +61,8 @@ SlashCmdList["WINLOSS"] = function(msg)
     WinLossDB = {}
   elseif command == "data" then
     DEFAULT_CHAT_FRAME:AddMessage("Fancy data visualizations coming soon");
+  elseif command == "debug" then
+    WinLossDB["debug"] = not WinLossDB["debug"];
+    DEFAULT_CHAT_FRAME:AddMessage("Debug is now "..tostring(WinLossDB["debug"]));
   end
 end
