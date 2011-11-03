f27b3f8af39580600c514e94ce91ed69d94c4f74
Jonas M Luster <j@d8c.us>
2011-11-03 16:50:51 -0500

    Broken Warning

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+**Note: The current version is broken a little, please don't install
+unless you're on git and can update. I'll push a new zip file later
+tonight**
+
 All you wanted (and never wanted) to know can be found at the
 [Wiki](https://github.com/jluster/WinLoss/wiki)
--- WinLoss.lua
+++ WinLoss.lua
@@ -5,13 +5,13 @@ wl:RegisterEvent('UPDATE_BATTLEFIELD_STATUS')
 wl:RegisterEvent('ADDON_LOADED')
 wl:RegisterEvent('WORLD_MAP_UPDATE')
 
-local currentBGsaved = 0
+wl.currentBGsaved = 0
 local uname,urealm = UnitName("player")
 
 function wl:WORLD_MAP_UPDATE()
   -- we can reasonably assume that the player has left the game or entered and new one and left
   -- the zone-in area (doesn't fire until you leave WSG hold, for example).
-  currentBGsaved = 0
+  self.currentBGsaved = 0
 end
 
 function wl:ADDON_LOADED()
@@ -27,11 +27,11 @@ function wl:UPDATE_BATTLEFIELD_STATUS()
     local u = UnitLevel("player");
     local s = GetNumBattlefieldScores();
     local zoneName = GetZoneText();
-    if currentBGsaved == 0 then
+    if self.currentBGsaved == 0 then
       -- Apparently sometimes zone changes aren't triggered so we'll also discount 
       -- any new win within 5 minutes after the last *sigh*
       WinLossDB[d..":"..uname] = winner..":"..u..":"..s..":"..zoneName;
-      currentBGsaved = 1
+      self.currentBGsaved = 1
     end
     DEFAULT_CHAT_FRAME:AddMessage(winner..":"..u..":"..s..":"..zoneName);
     if winner == 1 then 
